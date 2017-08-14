c2fc62643bee50089dac8da4cbb38cba9b0a2995
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-08-14 16:30:36 -0400

    addede models for category, good, blog_article

--- src/page/models.py
+++ src/page/models.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.TextField()
+
+
+class Good(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='Название')
+    in_stock = models.BooleanField(default=True, db_index=True, verbose_name='В наличии')
+    category = models.ForeignKey(Category, null=True, blank=True)
+
+
+class BlogArticle(models.Model):
+    title = models.CharField(max_length=240, unique_for_date='pubdate')
+    pubdate = models.DateField()
+    updated = models.DateTimeField(auto_now=True)
