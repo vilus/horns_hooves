fcde75c9ff0607088764a53e1d7eeb821ab02581
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-08-16 04:48:30 -0400

    added page models to admin site

--- src/page/admin.py
+++ src/page/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from page.models import Category, Good
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Good)
