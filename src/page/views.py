5de7a3a27a9e4017e9f652d86d7608bee7cc4a01
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-08-16 04:49:43 -0400

    added several views of page app

--- src/page/views.py
+++ src/page/views.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(req, category_id):
+    return HttpResponse('there is index or content of %s category' % category_id)
+
+
+def good(req, good_id):
+    return HttpResponse('there is %s good' % good_id)
