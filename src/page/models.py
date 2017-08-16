a3b4b49ad576ecb7376f29057ec59142e8eb6ef1
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-08-16 04:44:19 -0400

    changed models of page app

--- src/page/models.py
+++ src/page/models.py
@@ -8,11 +8,18 @@ class Category(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 
 class Good(models.Model):
     name = models.CharField(max_length=50, unique=True, verbose_name='Название')
+    description = models.TextField(default='')
     in_stock = models.BooleanField(default=True, db_index=True, verbose_name='В наличии')
-    category = models.ForeignKey(Category, null=True, blank=True)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
 
 
 class BlogArticle(models.Model):
