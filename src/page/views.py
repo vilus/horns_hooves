fefcd58103673a62834fd134595d184e49530f62
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-08-17 10:21:02 -0400

    page: added paginator for list of goods

--- src/page/views.py
+++ src/page/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import Http404
+from django.core.paginator import Paginator, InvalidPage
 from page.models import Category, Good
 
 
@@ -10,6 +11,8 @@ def index(req, category_id):
     cats = Category.objects.all().order_by('name')
     cat = Category.objects.get(pk=category_id) if category_id else Category.objects.first()
     goods = Good.objects.filter(category=cat).order_by('name')
+    paginator = Paginator(goods, 4)
+    goods = paginator.page(req.GET.get('page', 1))
     return render(req, 'page_index.html', {'category': cat, 'cats': cats, 'goods': goods})
 
 
@@ -19,4 +22,5 @@ def good(req, good_id):
         good = Good.objects.get(pk=good_id)
     except Good.DoesNotExist:
         raise Http404
-    return render(req, 'page_good.html', {'good': good, 'cats': cats})
+    pn = req.GET.get('page', 1)
+    return render(req, 'page_good.html', {'good': good, 'cats': cats, 'pn': pn})
