e94393eb5e378f273302ba39cd86bfe21b4b96ac
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-08-18 09:44:12 -0400

    page: refactored views to use django builtins views

--- src/page/cviews.py
+++ src/page/cviews.py
@@ -1,39 +1,43 @@
-from django.views.generic.base import TemplateView
-from django.core.paginator import Paginator, InvalidPage
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.base import ContextMixin
 from page.models import Category, Good
 
 
-class GoodListView(TemplateView):
-    template_name = 'page_index.html'
-
+class CategoryListMixin(ContextMixin):
     def get_context_data(self, **kwargs):
-        context = super(GoodListView, self).get_context_data(**kwargs)
-
-        context['cats'] = Category.objects.all().order_by('name')
-        if kwargs['category_id']:
-            context['category'] = Category.objects.get(id=kwargs['category_id'])
-        else:
-            context['category'] = Category.objects.first()
-
-        goods = Good.objects.filter(category=context['category']).order_by('name')
-        paginator = Paginator(goods, 4)
-        try:
-            context['goods'] = paginator.page(self.request.GET.get('page', 1))
-        except InvalidPage:
-            context['goods'] = paginator.page(1)
-
+        context = super(CategoryListMixin, self).get_context_data(**kwargs)
+        context['cats'] = Category.objects.order_by('name')
         return context
 
 
-class GoodDetailView(TemplateView):
+class GoodListView(ListView, CategoryListMixin):
+    template_name = 'page_index.html'
+    paginate_by = 3
+    cat = None
+
+    def get(self, request, *args, **kwargs):
+        if kwargs['category_id']:
+            self.cat = Category.objects.get(id=kwargs['category_id'])
+        else:
+            self.cat = Category.objects.first()
+        return super(GoodListView, self).get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(GoodListView, self).get_context_data(**kwargs)
+        context['category'] = self.cat
+        return context
+
+    def get_queryset(self):
+        return Good.objects.filter(category=self.cat).order_by('name')
+
+
+class GoodDetailView(DetailView, CategoryListMixin):
     template_name = 'page_good.html'
+    model = Good
+    pk_url_kwarg = 'good_id'
 
     def get_context_data(self, **kwargs):
         context = super(GoodDetailView, self).get_context_data(**kwargs)
-        context['cats'] = Category.objects.all().order_by("name")
-        try:
-            context['good'] = Good.objects.get(pk=kwargs['good_id'])
-        except Good.DoesNotExist:
-            raise Http404
         context['pn'] = self.request.GET.get('page', 1)
         return context
