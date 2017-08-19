db4c9cd80eeae9bec5e91ab60ec14a758b19d626
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-08-19 16:16:46 -0400

    page: added ability to create/edit/delete goods

--- src/page/cviews.py
+++ src/page/cviews.py
@@ -1,6 +1,9 @@
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views.generic.base import ContextMixin
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import ProcessFormView
+from django.core.urlresolvers import reverse
 from page.models import Category, Good
 
 
@@ -11,14 +14,74 @@ class CategoryListMixin(ContextMixin):
         return context
 
 
+class GoodEditMixin(CategoryListMixin):
+    def get_context_data(self, **kwargs):
+        context = super(GoodEditMixin, self).get_context_data(**kwargs)
+        context['pn'] = self.request.GET.get('page', 1)
+        return context
+
+
+class GoodEditView(ProcessFormView):
+    def post(self, request, *args, **kwargs):
+        pn = request.GET.get('page', '1')
+        self.success_url = self.success_url + '?page=' + pn
+        return super(GoodEditView, self).post(request *args, **kwargs)
+
+
+class GoodCreate(CreateView, GoodEditMixin):
+    model = Good
+    template_name = 'page_good_add.html'
+    fields = '__all__'
+
+    def get(self, request, *args, **kwargs):
+        if self.kwargs['cat_id'] is not None:
+            self.initial['category'] = Category.objects.get(pk=self.kwargs['cat_id'])
+        return super(GoodCreate, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        cat_id = Category.objects.get(pk=self.kwargs['cat_id']).id
+        # TODO: maybe to use just self.kwargs['cat_id'] without *.objects.get
+        self.success_url = reverse('page_index', kwargs={'cat_id': cat_id})
+        return super(GoodCreate, self).post(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super(GoodCreate, self).get_context_data(**kwargs)
+        context['category'] = Category.objects.get(pk=self.kwargs['cat_id'])
+        return context
+
+
+class GoodUpdate(UpdateView, GoodEditMixin, GoodEditView):
+    model = Good
+    template_name = 'page_good_edit.html'
+    pk_url_kwarg = 'good_id'
+    fields = '__all__'
+
+    def post(self, request, *args, **kwargs):
+        cat_id = Good.objects.get(pk=kwargs['good_id']).category.id
+        self.success_url = reverse('page_index', kwargs={'cat_id': cat_id})
+        return super(GoodUpdate, self).post(request, *args, **kwargs)
+
+
+class GoodDelete(DeleteView, GoodEditMixin, GoodEditView):
+    model = Good
+    template_name = 'page_good_delete.html'
+    pk_url_kwarg = 'good_id'
+    fields = '__all__'
+
+    def post(self, request, *args, **kwargs):
+        cat_id = Good.objects.get(pk=kwargs['good_id']).category.id
+        self.success_url = reverse('page_index', kwargs={'cat_id': cat_id})
+        return super(GoodDelete, self).post(request, *args, **kwargs)
+
+
 class GoodListView(ListView, CategoryListMixin):
     template_name = 'page_index.html'
     paginate_by = 3
     cat = None
 
     def get(self, request, *args, **kwargs):
-        if kwargs['category_id']:
-            self.cat = Category.objects.get(id=kwargs['category_id'])
+        if kwargs['cat_id']:
+            self.cat = Category.objects.get(id=kwargs['cat_id'])
         else:
             self.cat = Category.objects.first()
         return super(GoodListView, self).get(request, *args, **kwargs)
--- src/page/urls.py
+++ src/page/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from page.cviews import GoodListView, GoodDetailView
+from page.cviews import GoodListView, GoodDetailView, GoodCreate, GoodUpdate, GoodDelete
 
 
 urlpatterns = [
-    url(r'^(?:(?P<category_id>\d+)/)?$', GoodListView.as_view(), name='page_index'),
-    url(r'^good/(?P<good_id>\d+)/$', GoodDetailView.as_view(), name='good')
+    url(r'^(?:(?P<cat_id>\d+)/)?$', GoodListView.as_view(), name='page_index'),
+    url(r'^good/(?P<good_id>\d+)/$', GoodDetailView.as_view(), name='good'),
+    url(r'^(?P<cat_id>\d+)/add/$', GoodCreate.as_view(), name='good_add'),
+    url(r'^good/(?P<good_id>\d+)/edit/$', GoodUpdate.as_view(), name='good_edit'),
+    url(r'^good/(?P<good_id>\d+)/delete/$', GoodDelete.as_view(), name='good_delete'),
 ]
