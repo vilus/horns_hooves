aeb7cf5bd1009943f66260c192634df01b95c4b9
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-08-20 12:36:37 -0400

    page: added ability to use pictures for goods

--- src/page/models.py
+++ src/page/models.py
@@ -17,9 +17,24 @@ class Good(models.Model):
     description = models.TextField(default='')
     in_stock = models.BooleanField(default=True, db_index=True, verbose_name='В наличии')
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    thumbnail = models.ImageField(upload_to='goods/thumbnails', null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        try:
+            this_rec = Good.objects.get(pk=self.id)
+            if this_rec.thumbnail != self.thumbnail:
+                this_rec.thumbnail.delete(save=False)
+        except Exception:
+            # TODO: add to log
+            pass
+        super(Good, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        self.thumbnail.delete(save=False)
+        super(Good, self).delete(*args, **kwargs)
 
 
 class BlogArticle(models.Model):
