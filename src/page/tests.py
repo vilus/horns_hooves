ac53241d92add1ef7f8d77f757f99f140d642422
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-08-28 12:00:58 -0400

    page: add smoke tests just for check 200 code by urls
    Example run cmdline:
    ./manage.py test --debug-mode -v 2 page
    
    --debug-mode is needed becaous of django debug toolbar raises exceptions in non debug

--- src/page/tests.py
+++ src/page/tests.py
@@ -1,6 +1,32 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.urls import reverse
 from django.test import TestCase
 
-# Create your tests here.
+
+class SmokeTestCase(TestCase):
+    """
+    Just check status code of app urls
+    """
+
+    fixtures = ['test_data.json']
+    urls = [
+        {'viewname': 'page_index', 'kwargs': {'cat_id': 1}},
+        {'viewname': 'good', 'kwargs': {'good_id': 1}},
+        {'viewname': 'good_add', 'kwargs': {'cat_id': 2}},
+        {'viewname': 'good_edit', 'kwargs': {'good_id': 1}},
+        {'viewname': 'good_delete', 'kwargs': {'good_id': 1}},
+    ]
+
+    def setUp(self):
+        self.client.post('/login/', {'username': 'qa_staff', 'password': '!1q2w3e4R'})
+
+    def _check_status200(self, **reverse_params):
+        check_url = reverse(**reverse_params)
+        print('check status of url: {0}'.format(check_url))
+        res = self.client.get(check_url)
+        self.assertEqual(res.status_code, 200)
+
+for rev_params in SmokeTestCase.urls:
+    setattr(SmokeTestCase, 'test_' + rev_params['viewname'], lambda self, x=rev_params: self._check_status200(**x))
