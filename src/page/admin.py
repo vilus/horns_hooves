0098551e1fe6d7e2681df13e09ff0e6cf465feb9
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-10-24 11:25:09 -0400

    page: added support history in django admin

--- src/page/admin.py
+++ src/page/admin.py
@@ -2,8 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from simple_history.admin import SimpleHistoryAdmin
 from page.models import Category, Good
 
 
-admin.site.register(Category)
-admin.site.register(Good)
+class CategoryHistoryAdmin(SimpleHistoryAdmin):
+    list_display = ("name", "description")
+    search_fields = ('name', 'changed_by__username')
+    readonly_fields = ('changed_by', )
+
+admin.site.register(Category, CategoryHistoryAdmin)
+admin.site.register(Good, SimpleHistoryAdmin)
