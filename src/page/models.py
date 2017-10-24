85e482ffe1f8027eee2a0e191700f821fcef24a3
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-10-24 11:20:54 -0400

    page: add necessary fields to models and also connect history logging to signals post_vase, post_delete

--- src/page/models.py
+++ src/page/models.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 import logging
 from django.db import models
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 from django.core.validators import MinValueValidator
 from taggit.managers import TaggableManager
+from simple_history.models import HistoricalRecords
 
 logger = logging.getLogger('dev.'+__name__)
 
@@ -11,9 +14,19 @@ logger = logging.getLogger('dev.'+__name__)
 class Category(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.TextField()
+    changed_by = models.ForeignKey('auth.User', null=True, blank=True, on_delete=models.SET_NULL)
+    history = HistoricalRecords()
 
     def __str__(self):
         return self.name
+
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
 
 
 class Good(models.Model):
@@ -24,6 +37,8 @@ class Good(models.Model):
     thumbnail = models.ImageField(upload_to='goods/thumbnails', null=True, blank=True)
     tags = TaggableManager(blank=True, verbose_name='Теги')
     price = models.DecimalField(max_digits=8, decimal_places=2, default=0, validators=[MinValueValidator(0.0)])
+    changed_by = models.ForeignKey('auth.User', null=True, blank=True, on_delete=models.SET_NULL)
+    history = HistoricalRecords(excluded_fields=['tags'])
 
     def __str__(self):
         return self.name
@@ -44,8 +59,40 @@ class Good(models.Model):
         self.thumbnail.delete(save=False)
         super(Good, self).delete(*args, **kwargs)
 
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
+
 
 class BlogArticle(models.Model):
     title = models.CharField(max_length=240, unique_for_date='pubdate')
     pubdate = models.DateField()
     updated = models.DateTimeField(auto_now=True)
+
+
+@receiver(post_save, sender=Good, dispatch_uid='history-save-logger')
+@receiver(post_save, sender=Category, dispatch_uid='history-save-logger')
+def log_save_history(sender, instance, created, **_):
+    username = instance.changed_by.username if instance.changed_by else 'undefined'
+    if created:
+        logger.debug('<{0}>: "{1}" - was created by "{2}"'.format(sender.__name__, instance.name, username))
+        return
+
+    hist = instance.history.order_by('-history_date')[1]
+    model_fields = set(f.name for f in instance._meta.get_fields())
+    hist_fields = set(f.name for f in hist._meta.get_fields())
+    model_fields = model_fields & hist_fields
+    diff_fields = ['field "{0}" from "{1}" to "{2}"'.format(field, getattr(hist, field), getattr(instance, field))
+                   for field in model_fields if getattr(instance, field) != getattr(hist, field)]
+    logger.debug('<{0}>: "{1}" was changed by "{2}": {3}'.format(sender.__name__, instance.name, username, diff_fields))
+
+
+@receiver(post_delete, sender=Good, dispatch_uid='history-del-logger')
+@receiver(post_delete, sender=Category, dispatch_uid='history-del-logger')
+def log_del_history(sender, instance, **_):
+    username = instance.changed_by.username if instance.changed_by else 'undefined'
+    logger.debug('<{0}>: "{1}" - was deleted by "{2}"'.format(sender.__name__, instance.name, username))
