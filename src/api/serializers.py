4d6be238091244877cdba1bff93217e000b4f5c4
Vladimir Shevchenko <shevchenko_for_hr@mail.ru>
2017-10-24 11:26:40 -0400

    api: add necessary field to Category and Good serializers

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,14 +1,19 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from page.models import Category, Good
 
 
 class CategorySerializer(ModelSerializer):
+    changed_by = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Category
-        fields = ('__all__')
+        fields = ('id', 'name', 'description', 'changed_by')
 
 
 class GoodSerializer(ModelSerializer):
+    changed_by = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Good
-        fields = ('id', 'name', 'description', 'in_stock', 'category', 'price')
+        fields = ('id', 'name', 'description', 'in_stock', 'category', 'price', 'changed_by')
